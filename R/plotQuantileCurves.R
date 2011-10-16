44dfbd90065efdadb88a381c06b03abd574af90e
Tobias Verbeke <tobias.verbeke@openanalytics.eu>
2011-10-16 17:45:17 +0000

    aesthetic changes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MLP@59296 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotQuantileCurves.R
+++ R/plotQuantileCurves.R
@@ -13,32 +13,39 @@
 #' load(pathExampleMLPResult)
 #' @nord
 plotQuantileCurves <- function(x0, y0, hqi, xtp, qi, lqi, sym = TRUE, main = NULL) {
+ 
   mainTitle <- if (is.null(main)) "" else main
-  plot(x0, y0, xlab = "n", ylab = "MLP", axes = FALSE, col = "#08306B", pch = ".",
+  
+  darkBlue <- "#08306B"
+  someBlue <- "#2171B5"
+  someLightBlue <- "#539ECC"
+
+  plot(x0, y0, xlab = "n", ylab = "MLP", axes = FALSE, col = darkBlue, pch = ".",
       main = mainTitle)
-  axis(2, lwd = 1.5, col = "#08306B")
+  axis(2, lwd = 1.5, col = darkBlue)
   atPositions <- axis(1, labels = FALSE)
-  axis(1, lwd = 1.5, at = atPositions, labels = atPositions^2, las = 1, col = "#08306B")
-  par(cex = 0.5)
+  axis(1, lwd = 1.5, at = atPositions, labels = atPositions^2, las = 1, col = darkBlue)
+  op <- par(cex = 0.5)
   npc <- 16
-  ccc <- "#2171B5"
+  
   if (!is.null(hqi)){ 
     i2 <- y0 > xtp[,length(qi)]
-    points(x0[i2], y0[i2], col = ccc, pch = npc) 
+    points(x0[i2], y0[i2], col = someBlue, pch = npc) 
     if (length(i2[i2 == TRUE]) > 0){
-      text(x = x0[i2], y = jitter(y0[i2], factor=4), labels = names(x0[i2]), cex = 1.25, col = "#08306B")
+      text(x = x0[i2], y = jitter(y0[i2], factor=4), labels = names(x0[i2]), cex = 1.25, col = darkBlue)
     }
   }
   if (!is.null(lqi)){ 
     i2 <- y0 < xtp[,1] 
-    points(x0[i2], y0[i2], col = ccc, pch = npc) 
+    points(x0[i2], y0[i2], col = someBlue, pch = npc) 
   }
   if (sym){ 
-    i2 <- y0 < -xtp[,length(qi)] 
-    points(x0[i2], y0[i2], col = ccc,pch = npc) 
+    i2 <- y0 < -xtp[, length(qi)] 
+    points(x0[i2], y0[i2], col = someBlue,pch = npc) 
   }
-  par(cex = 1)
-  matlines(x0[i2 <- sort.list(x0)],xtp[i2,],col= "#539ECC",lty = 1)
+  par(op)
+  
+  matlines(x0[i2 <- sort.list(x0)], xtp[i2,], col = someLightBlue, lty = "solid")
   if (sym) 
-    matlines(x0[i2], -xtp[i2,], col= 3,lty = 1)
+    matlines(x0[i2], -xtp[i2,], col = 3, lty = "solid")
 }
